0369c87974024d685241d458ab4bb7f91cab3b70
ali-sageteck <ali@sage-teck.com>
2024-10-04 07:51:28 -0700

    images updated

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -12,8 +12,26 @@ class Pdf extends TCPDF
         // Logo
 		$this->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP+11, PDF_MARGIN_RIGHT);
 //        $image_file = FCPATH.'assets/images/pdf-header.png';
-        $image_file = FCPATH.'assets/images/pdf-full-bga4.jpg';
-        $this->Image($image_file, 0, 6, 210, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        $image_file = FCPATH.'assets/images/pdf-full-bga4.jpg';$this->Image(
+            $image_file,    // Path to the image
+            0,              // X position
+            6,              // Y position
+            510,            // Width (increase this value to make the image wider)
+            590,             // Height (you can set a value here to adjust height, or leave it empty for proportional scaling)
+            'JPG',          // Image format
+            '',             // Link
+            'T',            // Align ('T' for top)
+            false,          // Resize flag
+            300,            // DPI (dots per inch)
+            '',             // Align text to right
+            false,          // Image scale flag
+            false,          // Borders
+            0,              // Fit box
+            true,          // Fit on page
+            false,          // Output to browser
+            false           // Mask
+        );
+        
 //		$this->Image($image_file, 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
         // Set font
         $this->SetFont('helvetica', 'B', 20);
@@ -27,9 +45,9 @@ class Pdf extends TCPDF
 //        $this->SetY();
         // Set font
         $this->SetFont('helvetica', 'I', 8);
-		$image_file = FCPATH.'assets/images/pdf-footer.png';
-		$footer = $this->Image($image_file, 0,$this->GetY()-6, 210);
+	//	$image_file = FCPATH.'assets/images/pdf-footer.png';
+//		$footer = $this->Image($image_file, 0,$this->GetY()-6, 210);
         // Page number
-        $this->Cell(0, 20, $footer, 0, 0, 'R');
+ //       $this->Cell(0, 20, $footer, 0, 0, 'R');
     } 
 }
